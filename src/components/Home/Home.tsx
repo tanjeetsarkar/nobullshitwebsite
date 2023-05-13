247114775f0b69a6c46ca4d84e7d785819adb60e
Tanjeet Sarkar <tjspideydon@gmail.com>
2023-05-13 15:11:45 +0530

    Home responsiveness

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,18 +6,18 @@ import Biography from "../Biography/Biography";
 const Home: React.FC = () => {
   return (
       <div className="container-fluid d-flex flex-column">
-          <div className="d-sm-none">
+          <div className="d-sm-none d-flex align-items-center justify-content-center pb-3">
               <Avatar />
           </div>
       <div id="top-heading" className="top-heading">
         <h1 className="">
           <b>
-            Hi I'm XXXXX, a special human with ability to craft and build. I
+            Hi I'm Tanjeet, a special human with ability to craft and build. I
             turn ideas into brand.
           </b>
         </h1>
       </div>
-      <div className="d-flex">
+      <div className="d-flex mt-5 py-2 justify-content-sm-start justify-content-center align-items-center">
         <div className="d-none d-sm-block">
           <Avatar />
         </div>
