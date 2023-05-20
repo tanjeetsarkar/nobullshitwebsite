6f1890e08d4dc4dda72135a8a232ae5f4be47398
Tanjeet Sarkar <tjspideydon@gmail.com>
2023-05-20 17:16:09 +0530

    arrow button and arrow button functionality

--- src/components/Card/CardContainer.tsx
+++ src/components/Card/CardContainer.tsx
@@ -1,24 +1,25 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Card from "./Card";
 import "./CardContainer.css";
-
+import Arrow from "../../assets/arrow.svg";
 interface CardContainerProps {
     cards: any;
 }
 
 const CardContainer: React.FC<CardContainerProps> = ({ cards }) => {
-    const [scrollLeft, setScrollLeft] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(false);
     const containerRef = useRef<any>(null);
+    const cardRef = useRef<any>(null);
 
     const handleScroll = (scrollOffset: any) => {
         containerRef.current.scrollLeft += scrollOffset;
-        setScrollLeft(containerRef.current.scrollLeft);
+        setScrollLeft(containerRef.current.scrollLeft > 0);
     };
 
     return (
-        <div className="card-container" ref={containerRef}>
+        <div className="card-container mx-sm-2" ref={containerRef}>
             <div className="arrow left-arrow" onClick={() => handleScroll(-250)}>
-                <span>{"<-"}</span>
+                {scrollLeft && <img className="img-thumbnail" src={Arrow} style={{ transform: "rotate(180deg)", backgroundColor: "transparent", border: "none" }} />}
             </div>
             <div className="card-wrapper">
                 {cards.map((card: any, index: number) => (
@@ -26,7 +27,7 @@ const CardContainer: React.FC<CardContainerProps> = ({ cards }) => {
                 ))}
             </div>
             <div className="arrow right-arrow" onClick={() => handleScroll(250)}>
-                <span>{"->"}</span>
+                <img className="img-thumbnail" src={Arrow} style={{ backgroundColor: "transparent", border: "none" }} />
             </div>
         </div>
     );
